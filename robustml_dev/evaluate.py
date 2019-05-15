4f73a38358a558f53f073a1cf3b3ef90c9aa0fb8
czw0078 <czw0078@cirrascale>
2019-05-15 15:42:32 -0500

    add robustml framework

---
+++ robustml_dev/evaluate.py
@@ -0,0 +1,74 @@
+import numpy as np
+import sys
+import pickle
+import os
+
+def evaluate(model, attack, provider, start=None, end=None, deterministic=False, debug=False, snapshot_adv=False):
+    '''
+    Evaluate an attack on a particular model and return attack success rate._adv
+
+    An attack is allowed to be adaptive, so it's fine to design the attack
+    based on the specific model it's supposed to break.
+
+    `start` (inclusive) and `end` (exclusive) are indices to evaluate on. If
+    unspecified, evaluates on the entire dataset.
+
+    `deterministic` specifies whether to seed the RNG with a constant value for
+    a more deterministic test (so randomly selected target classes are chosen
+    in a pseudorandom way).
+    '''
+
+    if not provider.provides(model.dataset):
+        raise ValueError('provider does not provide correct dataset')
+    if start is not None and not (0 <= start < len(provider)):
+        raise ValueError('start value out of range')
+    if end is not None and not (0 <= end <= len(provider)):
+        raise ValueError('end value out of range')
+
+    threat_model = model.threat_model
+    targeted = threat_model.targeted
+
+    success = 0
+    total = 0
+    for i in range(start, end):
+        print('evaluating %d of [%d, %d)' % (i, start, end), file=sys.stderr)
+        total += 1
+        x_snap, x_orig, y, snapshot_filename = provider[i] 
+        target = None
+        if targeted: 
+            target = choose_target(i, y, model.dataset.labels, deterministic)
+        # x_adv = attack.run(np.copy(x), y, target)
+        x_adv = attack.run(np.copy(x_snap), np.copy(x_orig), y, target) 
+        if not threat_model.check(np.copy(x_orig), np.copy(x_adv)):
+            if debug:
+                print('check failed', file=sys.stderr)
+            continue
+        y_adv = model.classify(np.copy(x_adv))
+        if debug:
+            print('true = %d, adv = %d' % (y, y_adv), file=sys.stderr)
+        if targeted:
+            if y_adv == target:
+                success += 1
+        else: # false by default
+            if y_adv != y:
+                success += 1
+
+        # save adv to pkl file, can be run
+        if snapshot_filename != None and snapshot_adv:
+            os.makedirs(os.path.dirname(snapshot_filename), exist_ok=True)
+            pickle.dump(x_adv, open(snapshot_filename, 'wb'))
+
+    success_rate = success / total
+    return success_rate
+
+def choose_target(index, true_label, num_labels, deterministic=False):
+    if deterministic:
+        rng = np.random.RandomState(index)
+    else:
+        rng = np.random.RandomState()
+
+    target = true_label
+    while target == true_label:
+        target = rng.randint(0, num_labels)
+
+    return target
---
+++ robustml_dev/provider.py
@@ -0,0 +1,233 @@
+from abc import ABCMeta, abstractmethod
+import os
+import glob
+import numpy as np
+import pickle
+import gzip
+import PIL.Image
+
+from . import dataset as d
+
+class Provider(metaclass=ABCMeta):
+    @abstractmethod
+    def provides(self, dataset):
+        '''
+        Returns whether or not this provider can provide data for the given
+        dataset.
+        '''
+        raise NotImplementedError
+
+    @abstractmethod
+    def __len__(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def __getitem__(self, index):
+        '''
+        Returns a tuple (x, y) containing the data and label of the given
+        index.
+        '''
+        raise NotImplementedError
+
+class MNIST(Provider):
+    def __init__(self, image_path, label_path):
+        '''
+        image_path is a path to the 't10k-images-idx3-ubyte.gz' from
+        'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz'
+
+        label_path is a path to the 't10k-labels-idx1-ubyte.gz' from
+        'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz'
+        '''
+        with gzip.open(image_path, 'rb') as f:
+            images = f.read()
+        assert images[:4] == b'\x00\x00\x08\x03'
+        images = np.frombuffer(images[16:], dtype=np.uint8)
+        assert len(images) == 7840000
+        images = images.reshape((10000, 28, 28)).astype(np.float32) / 255.0
+        with gzip.open(label_path, 'rb') as f:
+            labels = f.read()
+        assert labels[:4] == b'\x00\x00\x08\x01'
+        labels = np.frombuffer(labels[8:], dtype=np.uint8)
+        assert len(labels) == 10000
+
+        self.xs = images
+        self.ys = labels
+
+    def provides(self, dataset):
+        return isinstance(dataset, d.MNIST)
+
+    def __len__(self):
+        return 10000
+
+    def __getitem__(self, index):
+        x = self.xs[index]
+        y = self.ys[index]
+        # add None for consistency
+        return x, y, None
+
+class CIFAR10(Provider):
+    def __init__(self, test_data):
+        '''
+        test_data is a path to the 'test_batch' file from
+        'http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+        '''
+        with open(test_data, 'rb') as f:
+            data = pickle.load(f, encoding='bytes')
+        self.xs = data[b'data'].reshape((10000,3,32,32)).astype(np.float32) / 255.0
+        self.xs = np.transpose(self.xs, (0,2,3,1)) # (N,3,32,32) -> (N,32,32,3)
+        self.ys = data[b'labels']
+
+    def provides(self, dataset):
+        return isinstance(dataset, d.CIFAR10)
+
+    def __len__(self):
+        return 10000
+
+    def __getitem__(self, index):
+        # add None for consistence
+        return self.xs[index], self.ys[index], None
+
+class ImageNet(Provider):
+    def __init__(self, path, shape):
+        self._path = path
+        self._shape = shape
+
+    def provides(self, dataset):
+        return isinstance(dataset, d.ImageNet) and self._shape == dataset.shape
+
+    def __len__(self):
+        return 50000
+
+    def __getitem__(self, index):
+        data_path = os.path.join(self._path, 'val')
+        image_paths = sorted([os.path.join(data_path, i) for i in os.listdir(data_path)])
+        assert len(image_paths) == 50000
+        labels_path = os.path.join(self._path, 'val.txt')
+        with open(labels_path) as labels_file:
+            labels = [i.split(' ') for i in labels_file.read().strip().split('\n')]
+            labels = {os.path.basename(i[0]): int(i[1]) for i in labels}
+        path = image_paths[index]
+        x = self._load_image(path)
+        y = labels[os.path.basename(path)]
+        # return x, y modified to add path
+        return x, y, None
+
+    # get centered crop of self._size
+    def _load_image(self, path):
+        h, w, c = self._shape
+        aspect = w / h
+        image = PIL.Image.open(path)
+        image_aspect = image.width / image.height
+
+        if image_aspect > aspect:
+            # image is wider than our aspect ratio
+            new_height = image.height
+            height_off = 0
+            new_width = int(aspect * new_height)
+            width_off = (image.width - new_width) // 2
+        else:
+            # image is taller than our aspect ratio
+            new_width = image.width
+            width_off = 0
+            new_height = int(new_width / aspect)
+            height_off = (image.height - new_height) // 2
+
+        # box is (left, upper, right, lower)
+        image = image.crop((
+            width_off,
+            height_off,
+            width_off+new_width,
+            height_off+new_height
+        ))
+
+        image = image.resize((w, h))
+
+        arr = np.asarray(image).astype(np.float32) / 255.0
+        if arr.ndim == 2:
+            # stack greyscale image
+            arr = np.repeat(arr[:,:,np.newaxis], repeats=3, axis=2)
+        if arr.shape[2] == 4:
+            # remove alpha channel
+            arr = arr[:,:,:3]
+        assert arr.shape == self._shape
+        return arr
+
+class ImageNet_val_1k(Provider): # change into ImageNet_val_1k
+    def __init__(self, path, shape):
+        self._path = path
+        self._shape = shape
+
+    def provides(self, dataset):
+        return isinstance(dataset, d.ImageNet) and self._shape == dataset.shape
+
+    def __len__(self):
+        return 1000
+
+    def get_image_item(self, index):
+        data_path = os.path.join(self._path, 'val')
+        image_paths = sorted([os.path.join(data_path, i) for i in os.listdir(data_path)])
+        assert len(image_paths) == 1000
+        labels_path = os.path.join(self._path, 'val.txt')
+        with open(labels_path) as labels_file:
+            labels = [i.split(' ') for i in labels_file.read().strip().split('\n')]
+            labels = {os.path.basename(i[0]): int(i[1]) for i in labels}
+        path = image_paths[index]
+        name = path.rsplit('.',1)[0].rsplit('/',2)[-1]
+        x = self._load_image(path)
+        y = labels[os.path.basename(path)]
+        return x, y, name        
+
+    def get_pickle_item(self, pkl_path): 
+        x = pickle.load(open(pkl_path, 'rb'))
+        return x 
+
+    def __getitem__(self, index):
+        x_orig, y, name = self.get_image_item(index)
+        snapshots = len(glob.glob(self._path+'/snapshot_*')) 
+        if snapshots > 0:
+            folder = self._path+'/snapshot_%04d'%snapshots
+            x_adv = self.get_pickle_item(folder+'/'+ name +'.pkl')
+        else:
+            x_adv = np.copy(x_orig)
+        new_folder = self._path + '/snapshot_%04d'%(snapshots+1)
+        return x_adv, x_orig, y, new_folder+'/'+ name +'.pkl' #
+
+    # get centered crop of self._size
+    def _load_image(self, path):
+        h, w, c = self._shape
+        aspect = w / h
+        image = PIL.Image.open(path)
+        image_aspect = image.width / image.height
+
+        if image_aspect > aspect:
+            # image is wider than our aspect ratio
+            new_height = image.height
+            height_off = 0
+            new_width = int(aspect * new_height)
+            width_off = (image.width - new_width) // 2
+        else:
+            # image is taller than our aspect ratio
+            new_width = image.width
+            width_off = 0
+            new_height = int(new_width / aspect)
+            height_off = (image.height - new_height) // 2
+
+        # box is (left, upper, right, lower)
+        image = image.crop((
+            width_off,
+            height_off,
+            width_off+new_width,
+            height_off+new_height
+        ))
+
+        image = image.resize((w, h))
+
+        arr = np.asarray(image).astype(np.float32) / 255.0
+        if arr.ndim == 2:
+            # stack greyscale image
+            arr = np.repeat(arr[:,:,np.newaxis], repeats=3, axis=2)
+        if arr.shape[2] == 4:
+            # remove alpha channel
+            arr = arr[:,:,:3]
+        assert arr.shape == self._shape
+        return arr
