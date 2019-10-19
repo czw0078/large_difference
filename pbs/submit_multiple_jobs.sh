1a2effbf249341ab25400929a8b2f6afdf97a949
cwang <czw0078@auburn.edu>
2019-10-19 14:07:11 -0500

    run experimrnt F to test if the attack and defense parameter not the same

--- pbs/submit_multiple_jobs.sh
+++ pbs/submit_multiple_jobs.sh
@@ -4,6 +4,6 @@ do
     let "i_end = i_start + 10"
     result_folder=$(printf 'parameter_%04d_%04d/snapshot_%04d/start_%04d' $2 $3 $4 $i_start)
     echo $1, start: $i_start, end: $i_end, defense: $2, attack: $3, output: $result_folder
-    qsub -v START=$i_start,END=$i_end,N_DEFENSE=$2,N_ATTACK=$3,RESULT_FOLDER=$result_folder $1
+    qsub -v START=$i_start,END=$i_end,N_DEFENSE=$2,N_ATTACK=$3,SNAPSHOT=$4,RESULT_FOLDER=$result_folder $1
     echo ""
 done
