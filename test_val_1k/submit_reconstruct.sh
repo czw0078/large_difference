d26c2067937edcfe4b2918083941175ff6dea6da
cwang <czw0078@auburn.edu>
2020-02-06 15:48:47 -0600

    generate the primitives

---
+++ test_val_1k/submit_reconstruct.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+qsub -v n=0200 reconstruct_template.pbs
+qsub -v n=0400 reconstruct_template.pbs
+qsub -v n=0600 reconstruct_template.pbs
+qsub -v n=0800 reconstruct_template.pbs
+sleep 4
+qsub -v n=1000 reconstruct_template.pbs
+qsub -v n=1200 reconstruct_template.pbs
+qsub -v n=1400 reconstruct_template.pbs
+qsub -v n=1600 reconstruct_template.pbs
+sleep 4
+qsub -v n=1800 reconstruct_template.pbs
+qsub -v n=2000 reconstruct_template.pbs
